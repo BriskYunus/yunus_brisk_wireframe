dc72290007a75bdc12bd14136aeed73e185d54b5
unknown <b_yunus@fanshaweonline.ca>
2019-01-21 19:52:04 -0500

    added files

---
+++ js/main.js
@@ -0,0 +1,11 @@
+//var firstName = "Phil";
+
+//alert("Hello " + firstName);
+ 
+// modern js
+
+//alert(`hello ${firstName} !!!`);
+
+//console.log(firstName);
+
+console.log("Javascript is linked up");
